e7ed6b7b8bcd131fe745bdc0832dcea771467546
ferdiLeCodeur <Ferdi_Codeur@protonmail.com>
2022-02-23 15:51:13 +0100

    ajout bouton + message + icon

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,28 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import {IonButton, IonContent, IonHeader, IonIcon, IonPage, IonTitle, IonToast, IonToolbar} from '@ionic/react';
 import './Home.css';
+import React, {useState} from "react";
+import {play as playIcon} from 'ionicons/icons';
 
 const Home: React.FC = () => {
+
+    const [showToast, setShowToast] = useState(false);
+    const handleClick = () => {
+        setShowToast(true);
+        setTimeout(()=>setShowToast(false), 1500);
+    };
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Mon application</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+          <IonButton color="primary" onClick={handleClick}>
+              <IonIcon icon={playIcon} slot ="start"></IonIcon>
+              Click Moi
+          </IonButton>
+          <IonToast isOpen={showToast} message ="Mon message!" ></IonToast>
       </IonContent>
     </IonPage>
   );
